5a0da82da68902d17f68d0fd2df2e6725533fbfa
Miles Roberts <milesdroberts@gmail.com>
2026-02-20 15:40:40 -0800

    try gt simulations with up-adjusted Ne

--- resources/06_sim_parameters.R
+++ resources/06_sim_parameters.R
@@ -7,12 +7,16 @@ params <- expand.grid(
     N=1000,
     L=5e6,
     n=50,
-    sigma=c(0, 0.1, 0.5, 0.9),
+    sigma=c(0, 0.05, 0.5, 0.95),
     alpha=c(0, 0.005, 0.01),
     gamma=5,
     tau=95,
-    rep=1:30
+    rep=1:30,
+    adjust=c(T, F)
 )
+
+# adjust Ne based on selfing rate
+params$N[(params$adjust == T)] = as.integer(params$N[(params$adjust == T)]/(1 - params$sigma[(params$adjust == T)]/2))
 
 # add simulation id
 params$ID <- 1:nrow(params)
