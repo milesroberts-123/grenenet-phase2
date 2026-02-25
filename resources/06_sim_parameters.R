2498bfafb351fe7b267019f903f38102473e185d
Miles Roberts <milesdroberts@gmail.com>
2026-02-24 17:09:30 -0800

    adjust sim params

--- resources/06_sim_parameters.R
+++ resources/06_sim_parameters.R
@@ -2,16 +2,16 @@ library("dplyr")
 
 params <- expand.grid(
     nmu=1e-8,
-    tmu=c(1e-9, 2e-9, 3e-9),
+    tmu=c(1e-9, 2e-9),
     R=1e-8,
     N=1000,
     L=5e6,
     n=50,
-    sigma=c(0, 0.05, 0.5, 0.95),
-    alpha=c(0, 0.005, 0.01),
+    sigma=c(0, 0.05, 0.5, 0.95, 1),
+    alpha=c(0, 0.01),
     gamma=5,
     tau=95,
-    rep=1:30,
+    rep=1:10,
     adjust=c(T, F)
 )
 
