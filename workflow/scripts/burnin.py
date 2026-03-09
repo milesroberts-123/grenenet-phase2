2580a6edd2354f6e123fe197b6b7e2da00580267
Miles Roberts <milesdroberts@gmail.com>
2026-03-09 15:53:35 -0700

    try to get individual and node times to calculate fst

--- workflow/scripts/burnin.py
+++ workflow/scripts/burnin.py
@@ -1,13 +1,14 @@
 import msprime
 import pyslim
-
+import tskit
+import numpy as np
 # Source - https://stackoverflow.com/a/16713986
 # Posted by Pierre GM, modified by community. See post 'Timeline' for change history
 # Retrieved 2026-03-06, License - CC BY-SA 4.0
 import argparse
 
 # Define the parser
-parser = argparse.ArgumentParser(description='Short sample app')
+parser = argparse.ArgumentParser(description='recapitate ')
 
 # Declare an argument (`--algo`), saying that the 
 # corresponding value should be stored in the `algo` 
@@ -17,6 +18,7 @@ parser.add_argument('--ID', action="store", dest='ID', default=0)
 parser.add_argument('-N', action="store", dest='N', default=0)
 parser.add_argument('-L', action="store", dest='L', default=0)
 parser.add_argument('-R', action="store", dest='R', default=0)
+parser.add_argument('--mu', action="store", dest='mu', default=0)
 
 # Now, parse the command line arguments and store the 
 # values in the `args` variable
@@ -26,8 +28,61 @@ args = parser.parse_args()
 print(args.ID)
 print(args.N)
 
-# Do msprime sim
-ts = msprime.sim_ancestry(samples=int(args.N), population_size=args.N,
-sequence_length=args.L, recombination_rate=args.R)
-slim_ts = pyslim.annotate(ts, model_type="WF", tick=1)
-slim_ts.dump("msprime_results/" + args.ID + ".trees")
+# load ts from slim
+print("Loading ts from slim...")
+slim_ts = tskit.load("slim_fst_results/" + args.ID + ".trees")
+print(slim_ts.metadata["SLiM"]["user_metadata"])
+
+print(f"The tree sequence has {slim_ts.num_trees} trees\n"
+      f"on a genome of length {slim_ts.sequence_length},\n"
+      f"{slim_ts.num_individuals} individuals, {slim_ts.num_samples} 'sample' genomes,\n"
+      f"and {slim_ts.num_mutations} mutations.")
+
+individual_times = slim_ts.individuals_time
+for t in np.unique(individual_times):
+    print(f"There are {np.sum(individual_times == t)} individuals from time {t}.")
+
+# recapitate ts
+print("Recapitating...")
+rts = pyslim.recapitate(slim_ts,
+             recombination_rate=args.R,
+             ancestral_Ne=int(args.N))
+
+# get nodes assigned to individuals
+#ind_nodes = np.array([ts.individual(x).nodes for x in range(ts.num_individuals)])
+#ts.nodes_individual
+#ts.nodes_time
+
+# get times assigned to individuals on nodes
+#node_times = np.array([ts.node(x).time for x in range(ts.num_nodes)])
+
+# get times for individuals
+#ind_times = np.array([node_times[x] for x in ind_nodes.flatten())
+
+# simplify ts
+#print("simplifying...")
+#keep_indivs = range(1000, 1003)
+#keep_nodes = []
+#for i in keep_indivs:
+#  keep_nodes.extend(rts.individual(i).nodes)
+
+#sts = rts.simplify(keep_nodes, keep_input_roots=True)
+
+# add mutations to ts
+print("Adding mutaitons...")
+next_id = pyslim.next_slim_mutation_id(rts)
+
+ts = msprime.sim_mutations(
+  rts,
+  rate=args.mu,
+  model=msprime.SLiMMutationModel(type=0, next_id=next_id),
+  keep=True,
+)
+
+# calculate fst between historical and modern
+#historical = []
+#modern = []
+print("Calculate fst...")
+result = ts.Fst(sample_sets=[ts.samples(time=(0,14)), ts.samples(time=(15, 191))])
+print(result)
+print("Done!")
